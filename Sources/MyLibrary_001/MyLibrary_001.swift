7eabf9827574fe6a5e547aa2c02a37a9158fabea
dslee02 <dslee@aladin.co.kr>
2022-05-03 14:40:14 +0900

    init spm

---
+++ Sources/MyLibrary_001/MyLibrary_001.swift
@@ -0,0 +1,8 @@
+public struct MyLibrary_001 {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+        
+        print("MyLibrary_001 \(text)")
+    }
+}
---
+++ Tests/MyLibrary_001Tests/MyLibrary_001Tests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import MyLibrary_001
+
+final class MyLibrary_001Tests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(MyLibrary_001().text, "Hello, World!")
+    }
+}
